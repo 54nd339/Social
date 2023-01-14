d1e9d09546d244dc14b079adab649be9f220b219
54nd339 <20051025@kiit.ac.in>
2023-01-15 03:24:38 +0530

    netlify fix(maybe)

--- next.config.js
+++ next.config.js
@@ -4,4 +4,5 @@ module.exports = {
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dhc5mer1w/image/upload",
   }, //url for image upload to cloudinary. First copy "API base url" from Cloudinary. Then paste it here.
   staticPageGenerationTimeout: 1000,
+  target: "serverless",
 };
