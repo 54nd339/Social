d252f9af2acfc560734cd45f363ab8f4a841d8eb
54nd339 <20051025@kiit.ac.in>
2023-01-18 16:39:33 +0530

    node version correction

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://socialfair.netlify.app";
+    : "http://localhost:3000"
+    // "https://socialfair.netlify.app";
 
 export default baseUrl;
